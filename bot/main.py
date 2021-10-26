328f741c3249219eeb67b7f8b342ea061c895d99
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-26 23:57:34 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,5 @@
 import discord
-#from discord.ext import commands
+from discord.ext import commands
 
 from discord_slash import SlashCommand
 from discord_slash.utils.manage_commands import create_option
