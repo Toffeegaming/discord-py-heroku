384c5a5776aeb0da516884be3edf04bd281864be
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-27 00:49:27 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -15,19 +15,8 @@ guild_ids = [ int(os.getenv("GUILD1")), int(os.getenv("GUILD2")), int(os.getenv(
 bot = Bot(command_prefix=os.getenv("DISCORD_PREFIX"), help_command=None, description=os.getenv("DISCORD_DESCRIPTION"), intents=discord.Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
 
-# Load cogs
-def LoadCogs():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    for filename in os.listdir(dir_path + '/cogs'):
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}')
-            print(f"[COGS] loaded {filename[:-3]}")
-        else:
-            print(f'[COGS] Unable to load {filename[:-3]}')
-
 @bot.event 
 async def on_ready():
-    LoadCogs()
     print(f"Logged in as {bot.user.name}({bot.user.id})")
     await bot.change_presence(activity=discord.Game(name='with my feelings'),status=discord.Status.online)
     owner = await bot.fetch_user(OWNER)
@@ -107,32 +96,16 @@ async def _Geil(ctx):
         await ctx.send(mention + " is een sexy beast")
 
 
-# from mcstatus import MinecraftServer
 
-# MCServer = [int(os.getenv("GUILD2")),int(os.getenv("GUILD3"))]
 
-# @slash.slash( #server status
-#     name='Server',
-#     description='Check of de minecraft server online is.',
-#     guild_ids = MCServer)
-# async def _Server(ctx):
-#     print(f"Started running server command")
-#     try:
-#         ip = os.getenv("SERVER")
-#         server = MinecraftServer(ip,25565)
-#         query = server.query()
-#         placeholder = "{names}"
-#         test = placeholder.format( names = ", ".join(query.players.names) )
-#         if test:
-#             names = "Deze mensen zijn op de server: " + test
-#         else:
-#             names = "Niemand is online"
-#         intro = "De server is online!\n"
-#         message = intro + names
-#         await ctx.send( message )
-
-#     except:
-#         await ctx.send("Server is offline")
+# Load cogs
+dir_path = os.path.dirname(os.path.realpath(__file__))
+for filename in os.listdir(dir_path + '/cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+        print(f"[COGS] loaded {filename[:-3]}")
+    else:
+        print(f'[COGS] Unable to load {filename[:-3]}')
 
 # Create bot
 bot.run(TOKEN)
