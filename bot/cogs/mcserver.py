a0baeabbc38b3c4af3bd6c1115021293ecc375a5
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-27 00:05:59 +0200

    Update mcserver.py

--- bot/cogs/mcserver.py
+++ bot/cogs/mcserver.py
@@ -1,5 +1,6 @@
-#import discord
-from discord_slash import SlashCommand, cog_ext, SlashContext
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+
 from mcstatus import MinecraftServer
 
 class Minecraft(commands.Cog)
