e9ebca27512baff3fd36a5030b54e2afc52a1230
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-30 21:32:42 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,32 +1,37 @@
+import os
 import discord
 from discord.ext.commands import Bot
 from discord_slash import SlashCommand
-import os
-import datetime
+
+import datetime # timestamp discord log output
+import asyncio # handle sync calling of await
 
 bot = Bot(command_prefix=os.getenv("DISCORD_PREFIX"), help_command=None, description=os.getenv("DISCORD_DESCRIPTION"), intents=discord.Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
 
+async def PrintToDiscord(message = ""):
+    time = datetime.datetime.utcnow()
+    await bot.get_channel( int(os.getenv("LOGS")) ).send(f"[{time}] {message}")
+
+loop = asyncio.get_event_loop()
+
 @bot.event 
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
-    time = datetime.datetime.now()
+    time = datetime.datetime.utcnow()
     await bot.get_channel( int(os.getenv("LOGS")) ).send(f"[{time}] [STARTUP] Logged in!")
     await bot.change_presence(activity=discord.Game(name='with my feelings'),status=discord.Status.online)
-
 
 # load cogs
 dir_path = os.path.dirname(os.path.realpath(__file__))
 for filename in os.listdir(dir_path + '/cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
+        coroutine = PrintToDiscord(f"[COGS] loaded {filename[:-3]}")
+        loop.run_until_complete(coroutine)
         print(f"[COGS] loaded {filename[:-3]}")
     else:
         print(f'[COGS] Unable to load {filename[:-3]}')
 
-
-
 # Create bot
 bot.run(os.getenv("DISCORD_TOKEN"))
-
-
