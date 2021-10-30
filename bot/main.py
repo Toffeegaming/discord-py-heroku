b380daf3b435d3eef988f96ec5f4a323e1f79319
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-30 19:02:17 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -9,13 +9,13 @@ bot = Bot(command_prefix=os.getenv("DISCORD_PREFIX"), help_command=None, descrip
 slash = SlashCommand(bot, sync_commands=True)
 
 async def PrintToLogChannel(message):
+    print(message)
     logChannel = bot.get_channel(os.getenv("LOGS"))
     await logChannel.message.send(message)
 
 @bot.event 
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
-    PrintToLogChannel(f"Logged in as {bot.user.name}({bot.user.id})")
+    await PrintToLogChannel(f"Logged in as {bot.user.name}({bot.user.id})")
     await bot.change_presence(activity=discord.Game(name='with my feelings'),status=discord.Status.online)
 
 # Load cogs
@@ -23,11 +23,9 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 for filename in os.listdir(dir_path + '/cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
-        print(f"[COGS] loaded {filename[:-3]}")
-        PrintToLogChannel(f"[COGS] loaded {filename[:-3]}")
+        await PrintToLogChannel(f"[COGS] loaded {filename[:-3]}")
     else:
-        print(f'[COGS] Unable to load {filename[:-3]}')
-        PrintToLogChannel(f'[COGS] Unable to load {filename[:-3]}')
+        await PrintToLogChannel(f'[COGS] Unable to load {filename[:-3]}')
 
 # Create bot
 bot.run(os.getenv("DISCORD_TOKEN"))
