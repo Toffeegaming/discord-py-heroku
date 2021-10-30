ed801f92f7abd23d1c06c209e1578ec38d2a2fd0
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-30 17:59:52 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -11,17 +11,17 @@ logID = os.getenv("LOGS")
 bot = Bot(command_prefix=os.getenv("DISCORD_PREFIX"), help_command=None, description=os.getenv("DISCORD_DESCRIPTION"), intents=discord.Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
 
-logChannel = bot.get_channel(logID)
-
 @bot.event 
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
+    logChannel = bot.get_channel(logID)
     await logChannel.message.send(f"Logged in as {bot.user.name}({bot.user.id})")
     await bot.change_presence(activity=discord.Game(name='with my feelings'),status=discord.Status.online)
 
 # Load cogs
 dir_path = os.path.dirname(os.path.realpath(__file__))
 for filename in os.listdir(dir_path + '/cogs'):
+    logChannel = bot.get_channel(logID)
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
         print(f"[COGS] loaded {filename[:-3]}")
