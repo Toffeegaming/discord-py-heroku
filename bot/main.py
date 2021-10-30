30afd6bd8f9b20abf2bedbe0f1fe34a98ac9abe9
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-30 19:09:10 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,7 @@ import os
 bot = Bot(command_prefix=os.getenv("DISCORD_PREFIX"), help_command=None, description=os.getenv("DISCORD_DESCRIPTION"), intents=discord.Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
 
-def PrintToLogChannel(message):
+async def PrintToLogChannel(message):
     print(message)
     logChannel = bot.get_channel(os.getenv("LOGS"))
     await logChannel.message.send(message)
@@ -18,14 +18,17 @@ async def on_ready():
     await PrintToLogChannel(f"Logged in as {bot.user.name}({bot.user.id})")
     await bot.change_presence(activity=discord.Game(name='with my feelings'),status=discord.Status.online)
 
-# Load cogs
-dir_path = os.path.dirname(os.path.realpath(__file__))
-for filename in os.listdir(dir_path + '/cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-        PrintToLogChannel(f"[COGS] loaded {filename[:-3]}")
-    else:
-        PrintToLogChannel(f'[COGS] Unable to load {filename[:-3]}')
+async def load_cogs():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    for filename in os.listdir(dir_path + '/cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
+            await PrintToLogChannel(f"[COGS] loaded {filename[:-3]}")
+        else:
+            await PrintToLogChannel(f'[COGS] Unable to load {filename[:-3]}')
+
+# load cogs
+await load_cogs()
 
 # Create bot
 bot.run(os.getenv("DISCORD_TOKEN"))
