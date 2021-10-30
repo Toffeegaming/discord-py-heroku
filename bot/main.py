37520d4c470ae0258f74edf5738434200b13e2d4
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-30 19:06:11 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,7 @@ import os
 bot = Bot(command_prefix=os.getenv("DISCORD_PREFIX"), help_command=None, description=os.getenv("DISCORD_DESCRIPTION"), intents=discord.Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
 
-async def PrintToLogChannel(message):
+def PrintToLogChannel(message):
     print(message)
     logChannel = bot.get_channel(os.getenv("LOGS"))
     await logChannel.message.send(message)
@@ -23,9 +23,9 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 for filename in os.listdir(dir_path + '/cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
-        await PrintToLogChannel(f"[COGS] loaded {filename[:-3]}")
+        PrintToLogChannel(f"[COGS] loaded {filename[:-3]}")
     else:
-        await PrintToLogChannel(f'[COGS] Unable to load {filename[:-3]}')
+        PrintToLogChannel(f'[COGS] Unable to load {filename[:-3]}')
 
 # Create bot
 bot.run(os.getenv("DISCORD_TOKEN"))
