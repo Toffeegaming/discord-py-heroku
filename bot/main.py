61f9c3d266381ec21c3f0be972c16b8b82f28658
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-30 17:58:11 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,13 +6,17 @@ from discord_slash import SlashCommand
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+logID = os.getenv("LOGS")
 
 bot = Bot(command_prefix=os.getenv("DISCORD_PREFIX"), help_command=None, description=os.getenv("DISCORD_DESCRIPTION"), intents=discord.Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
 
+logChannel = bot.get_channel(logID)
+
 @bot.event 
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
+    await logChannel.message.send(f"Logged in as {bot.user.name}({bot.user.id})")
     await bot.change_presence(activity=discord.Game(name='with my feelings'),status=discord.Status.online)
 
 # Load cogs
@@ -21,8 +25,10 @@ for filename in os.listdir(dir_path + '/cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
         print(f"[COGS] loaded {filename[:-3]}")
+        await logChannel.message.send(f"[COGS] loaded {filename[:-3]}")
     else:
         print(f'[COGS] Unable to load {filename[:-3]}')
+        await logChannel.message.send(f'[COGS] Unable to load {filename[:-3]}')
 
 # Create bot
 bot.run(TOKEN)
