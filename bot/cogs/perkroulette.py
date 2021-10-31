44f4d9cc5767ed14893bb6b635eeea7c97c7d1f6
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-31 20:00:44 +0100

    Update perkroulette.py

--- bot/cogs/perkroulette.py
+++ bot/cogs/perkroulette.py
@@ -119,6 +119,8 @@ class Roulette(Cog):
     ]
 
     def SelectPerks(self, in_id, in_range):
+        test = "{"jsonrpc": "2.0","method": "generateIntegerSequences","params": {"apiKey": os.getenv("RANDOM-API"),"n": 1,"length": 4,"min": 0,"max": in_range,"replacement": false,"base": 10},"id": in_id}"
+        
         request = {
             "jsonrpc": "2.0",
             "method": "generateIntegerSequences",
@@ -134,8 +136,9 @@ class Roulette(Cog):
             "id": in_id
         }
 
+        #data=json.dumps(request),
         response = requests.post('https://api.random.org/json-rpc/4/invoke',
-        data=json.dumps(request),
+        data=json.dumps(test),
         headers={'content-type': 'application/json'})
 
         data = response.json()
