6a1de5d8efa24bd3cced8119fa61ee0d66b4204b
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-10-31 19:42:49 +0100

    Update perkroulette.py

--- bot/cogs/perkroulette.py
+++ bot/cogs/perkroulette.py
@@ -11,113 +11,114 @@ from discord_slash.utils.manage_commands import generate_options
 
 guild_ids = [int(os.getenv("GUILD2")), int(os.getenv("GUILD3"))]
 
-with suppress(NameError):
 
-    class Roulette(Cog):
-        def __init__(self, bot: Bot):
-            self.bot = bot
 
-        SurvivorPerks = [ ######### https://deadbydaylight.fandom.com/wiki/Perks
-            "Ace in the Hole",
-            "Adrenaline",
-            "Aftercare",
-            "Alert",
-            "Any Means Necessary",
-            "Appraisal",
-            "Autodidact",
-            "Balanced Landing",
-            "Bite the Bullet",
-            "Blast Mine",
-            "Blood Pact",
-            "Boil Over",
-            "Bond",
-            "Boon: Circle of Healing",
-            "Boon: Shadow Step",
-            "Borrowed Time",
-            "Botany Knowledge",
-            "Breakdown",
-            "Breakout",
-            "Buckle Up",
-            "Built to Last",
-            "Calm Spirit",
-            "Clairvoyance",
-            "Counterforce",
-            "Dance With Me",
-            "Dark Sense",
-            "Dead Hard",
-            "Deception",
-            "Decisive Strike",
-            "Deliverance",
-            "Desperate Measures",
-            "Detective's Hunch",
-            "Distortion",
-            "Diversion",
-            "Déjà Vu",
-            "Empathy",
-            "Fast Track",
-            "Flashbang",
-            "Flip-Flop",
-            "For the People",
-            "Guardian",
-            "Head On",
-            "Hope",
-            "Inner Healing",
-            "Iron Will",
-            "Kindred",
-            "Kinship",
-            "Leader",
-            "Left Behind",
-            "Lightweight",
-            "Lithe",
-            "Lucky Break",
-            "Mettle of Man",
-            "No Mither",
-            "No One Left Behind",
-            "Object of Obsession",
-            "Off the Record",
-            "Open-Handed",
-            "Pharmacy",
-            "Plunderer's Instinct",
-            "Poised",
-            "Power Struggle",
-            "Premonition",
-            "Prove Thyself",
-            "Quick & Quiet",
-            "Red Herring",
-            "Renewal",
-            "Repressed Alliance",
-            "Resilience",
-            "Resurgence",
-            "Rookie Spirit",
-            "Saboteur",
-            "Self-Aware",
-            "Self-Care",
-            "Self-Preservation",
-            "Situational Awareness",
-            "Slippery Meat",
-            "Small Game",
-            "Smash Hit",
-            "Sole Survivor",
-            "Solidarity",
-            "Soul Guard",
-            "Spine Chill",
-            "Sprint Burst",
-            "Stake Out",
-            "Streetwise",
-            "Technician",
-            "Tenacity",
-            "This Is Not Happening",
-            "Unbreakable",
-            "Up the Ante",
-            "Urban Evasion",
-            "Vigil",
-            "Visionary",
-            "Wake Up!",
-            "We'll Make It",
-            "We're Gonna Live Forever",
-            "Windows of Opportunity"
-        ]
+class Roulette(Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
 
+    SurvivorPerks = [ ######### https://deadbydaylight.fandom.com/wiki/Perks
+        "Ace in the Hole",
+        "Adrenaline",
+        "Aftercare",
+        "Alert",
+        "Any Means Necessary",
+        "Appraisal",
+        "Autodidact",
+        "Balanced Landing",
+        "Bite the Bullet",
+        "Blast Mine",
+        "Blood Pact",
+        "Boil Over",
+        "Bond",
+        "Boon: Circle of Healing",
+        "Boon: Shadow Step",
+        "Borrowed Time",
+        "Botany Knowledge",
+        "Breakdown",
+        "Breakout",
+        "Buckle Up",
+        "Built to Last",
+        "Calm Spirit",
+        "Clairvoyance",
+        "Counterforce",
+        "Dance With Me",
+        "Dark Sense",
+        "Dead Hard",
+        "Deception",
+        "Decisive Strike",
+        "Deliverance",
+        "Desperate Measures",
+        "Detective's Hunch",
+        "Distortion",
+        "Diversion",
+        "Déjà Vu",
+        "Empathy",
+        "Fast Track",
+        "Flashbang",
+        "Flip-Flop",
+        "For the People",
+        "Guardian",
+        "Head On",
+        "Hope",
+        "Inner Healing",
+        "Iron Will",
+        "Kindred",
+        "Kinship",
+        "Leader",
+        "Left Behind",
+        "Lightweight",
+        "Lithe",
+        "Lucky Break",
+        "Mettle of Man",
+        "No Mither",
+        "No One Left Behind",
+        "Object of Obsession",
+        "Off the Record",
+        "Open-Handed",
+        "Pharmacy",
+        "Plunderer's Instinct",
+        "Poised",
+        "Power Struggle",
+        "Premonition",
+        "Prove Thyself",
+        "Quick & Quiet",
+        "Red Herring",
+        "Renewal",
+        "Repressed Alliance",
+        "Resilience",
+        "Resurgence",
+        "Rookie Spirit",
+        "Saboteur",
+        "Self-Aware",
+        "Self-Care",
+        "Self-Preservation",
+        "Situational Awareness",
+        "Slippery Meat",
+        "Small Game",
+        "Smash Hit",
+        "Sole Survivor",
+        "Solidarity",
+        "Soul Guard",
+        "Spine Chill",
+        "Sprint Burst",
+        "Stake Out",
+        "Streetwise",
+        "Technician",
+        "Tenacity",
+        "This Is Not Happening",
+        "Unbreakable",
+        "Up the Ante",
+        "Urban Evasion",
+        "Vigil",
+        "Visionary",
+        "Wake Up!",
+        "We'll Make It",
+        "We're Gonna Live Forever",
+        "Windows of Opportunity"
+    ]
+
+    with suppress(NameError):
         def SelectPerks(self, in_id, in_range):
             request = {
                 "jsonrpc": "2.0",
@@ -147,18 +148,18 @@ with suppress(NameError):
             generatedList = rand.sample(range(in_range),4)
             return generatedList
 
-        def get_data(self):
-            with open("roulette_userdata.json", 'r') as file:
-                return json.loads(file.read())
+    def get_data(self):
+        with open("roulette_userdata.json", 'r') as file:
+            return json.loads(file.read())
 
-        @cog_ext.cog_slash(name='Survivor', description='Krijg 4 random survivor perks!', guild_ids=guild_ids)
-        async def _Survivor(self,ctx: SlashContext):
-            generatedPerks = self.SelectPerks(ctx.author_id, 97)
-            await ctx.send(f"{ctx.author.name} krijgt:{os.linesep}{self.SurvivorPerks[generatedPerks[0]]}{os.linesep}{self.SurvivorPerks[generatedPerks[1]]}{os.linesep}{self.SurvivorPerks[generatedPerks[2]]}{os.linesep}{self.SurvivorPerks[generatedPerks[3]]}")
+    @cog_ext.cog_slash(name='Survivor', description='Krijg 4 random survivor perks!', guild_ids=guild_ids)
+    async def _Survivor(self,ctx: SlashContext):
+        generatedPerks = self.SelectPerks(ctx.author_id, 97)
+        await ctx.send(f"{ctx.author.name} krijgt:{os.linesep}{self.SurvivorPerks[generatedPerks[0]]}{os.linesep}{self.SurvivorPerks[generatedPerks[1]]}{os.linesep}{self.SurvivorPerks[generatedPerks[2]]}{os.linesep}{self.SurvivorPerks[generatedPerks[3]]}")
 
-        @cog_ext.cog_slash(name='Killer', description='Krijg 4 random killer perks!', guild_ids=guild_ids)
-        async def _Killer(self,ctx: SlashContext):
-            await ctx.send(f"Under construction :)")
+    @cog_ext.cog_slash(name='Killer', description='Krijg 4 random killer perks!', guild_ids=guild_ids)
+    async def _Killer(self,ctx: SlashContext):
+        await ctx.send(f"Under construction :)")
 
-    def setup(bot: Bot):
-        bot.add_cog( Roulette(bot) )
+def setup(bot: Bot):
+    bot.add_cog( Roulette(bot) )
