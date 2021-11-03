3aa97c1d1a8a681041dabdfc1840e4f7d5f08484
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-11-03 15:24:16 +0100

    Update perkroulette.py

--- bot/cogs/perkroulette.py
+++ bot/cogs/perkroulette.py
@@ -617,7 +617,7 @@ class Roulette(Cog):
         self.check_connection()
         await self.PerkMaker(ctx,'Killer')
 
-    @cog_ext.cog_component_callback()
+    @cog_ext.cog_component()
     async def hello(ctx: SlashContext):
         await ctx.edit_origin(content="You pressed a button!")
 
