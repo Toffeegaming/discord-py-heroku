81909720264b6fbd3a83a4f9ff2e34103ac004ce
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2021-11-18 13:13:54 +0100

    Update ping.py

--- bot/cogs/ping.py
+++ bot/cogs/ping.py
@@ -3,13 +3,19 @@ import os
 from discord.ext.commands import Bot, Cog
 from discord_slash import cog_ext, SlashContext
 
-guild_ids = [int(os.getenv("GUILD2")), int(os.getenv("GUILD3")) ]
-
 class Ping(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
+        self.guild_ids = _getGuilds()
+    
+    def _getGuilds(self):
+        list = []
+        for guild in self.bot.guilds:
+            list.append(guild.id)
+        print(list)
+        return list
 
-    @cog_ext.cog_slash(name='Ping', description='Pong!', guild_ids=guild_ids)
+    @cog_ext.cog_slash(name='Ping', description='Pong!', guild_ids=self.guild_ids)
     async def _Ping(self,ctx: SlashContext):
         await ctx.send(f"Pong! ({self.bot.latency*1000}ms)")
 
