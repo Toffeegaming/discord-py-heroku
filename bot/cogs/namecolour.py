25e648cc8d11b7fc2ce4a2653df3254c392fc061
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2022-03-25 16:06:56 +0100

    Update namecolour.py

--- bot/cogs/namecolour.py
+++ bot/cogs/namecolour.py
@@ -47,7 +47,7 @@ class Kleur(interactions.Extension):
             input.ljust(8)
 
             currentGuild = interactions.Guild(**await self.client.http().get_guild(956152709034164224, client=self.client.http()))
-            user_role_id = self.data.index(int(ctx.author_id)) + 1
+            user_role_id = self.data.index(int(ctx.author_id) + 1)
 
             await currentGuild.modify_role(role_id=user_role_id, color = input, reason="Deze persoon wilde een andere kleur")
             await ctx.send(f"Kleur veranderd in {input}")
