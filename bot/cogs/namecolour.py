393706e5c2881663a98eac59d96e37384b8409c4
Toffee <74181914+Toffeegaming@users.noreply.github.com>
2022-03-25 17:03:11 +0100

    Update namecolour.py

--- bot/cogs/namecolour.py
+++ bot/cogs/namecolour.py
@@ -46,7 +46,7 @@ class Kleur(interactions.Extension):
                 input.replace('#','0x')
             input.ljust(8)
 
-            currentGuild = interactions.Guild(**await self.client._http.get_guild(956152709034164224), _client = self.client._http)
+            currentGuild = interactions.Guild(**await self.client._http.get_guild(956152709034164224, _client = self.client._http) )
 
             user_id_index = int( self.data.index( int( ctx.author.id) ) )
             print(user_id_index)
