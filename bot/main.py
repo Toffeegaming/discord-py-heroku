1ec9731f88dd7ec27bdbc0fba7c4abddb55e5375
Cas Olischläger <74181914+Toffeegaming@users.noreply.github.com>
2022-04-09 18:28:30 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -40,65 +40,65 @@ bot = interactions.Client(
     ), 
     disable_sync=False)
 
-list_guild_ids = []
+# list_guild_ids = []
 
-async def getNumberGuilds():
-    guild_list = await bot._http.get_self_guilds()
-    for guild in guild_list:
-        list_guild_ids.append( int( guild["id"] ) )
+# async def getNumberGuilds():
+#     guild_list = await bot._http.get_self_guilds()
+#     for guild in guild_list:
+#         list_guild_ids.append( int( guild["id"] ) )
 
-@bot.event 
-async def on_ready():
-    print(f"Logged in as {bot.me.name}({bot.me.id})")
-    channel = interactions.Channel(**await bot._http.get_channel( int( os.getenv("LOGS")) ), _client=bot._http)
+# @bot.event 
+# async def on_ready():
+#     print(f"Logged in as {bot.me.name}({bot.me.id})")
+#     channel = interactions.Channel(**await bot._http.get_channel( int( os.getenv("LOGS")) ), _client=bot._http)
 
-    await getNumberGuilds()
-    numberGuild = len(list_guild_ids)
-    sheet.update_acell(f'{ str(os.getenv("DISCORD_SERVERCOUNT") ) }',str(numberGuild))
+#     await getNumberGuilds()
+#     numberGuild = len(list_guild_ids)
+#     sheet.update_acell(f'{ str(os.getenv("DISCORD_SERVERCOUNT") ) }',str(numberGuild))
 
-    time = datetime.datetime.utcnow()
-    await channel.send(f"[{time}] [STARTUP] Logged in in {numberGuild} servers!{os.linesep}{list_guild_ids}")
+#     time = datetime.datetime.utcnow()
+#     await channel.send(f"[{time}] [STARTUP] Logged in in {numberGuild} servers!{os.linesep}{list_guild_ids}")
 
-@bot.event
-async def on_guild_member_add(ctx):
-    guild_id = int( ctx.guild_id )
-    print(guild_id)
-    if not guild_id == 956152709034164224:
-        print("GuidId incorrect")
-        return
-    print("GuidId correct")
+# @bot.event
+# async def on_guild_member_add(ctx):
+#     guild_id = int( ctx.guild_id )
+#     print(guild_id)
+#     if not guild_id == 956152709034164224:
+#         print("GuidId incorrect")
+#         return
+#     print("GuidId correct")
 
-    googleData = CreateGspread()
-    sheet = googleData.worksheet("RoleData")
+#     googleData = CreateGspread()
+#     sheet = googleData.worksheet("RoleData")
 
-    row = sheet.find(str(ctx.user.id))
+#     row = sheet.find(str(ctx.user.id))
 
-    if row is not None:
-        print("userdata exists")
-        return
-    print("userdata does not exist")
+#     if row is not None:
+#         print("userdata exists")
+#         return
+#     print("userdata does not exist")
 
-    counter = int( sheet.acell(f'C1').value ) + 1
+#     counter = int( sheet.acell(f'C1').value ) + 1
 
-    sheet.update_acell(f'A{counter}',str(ctx.user.id))
+#     sheet.update_acell(f'A{counter}',str(ctx.user.id))
 
-    roleData = {
-        "name" : str(ctx.user.username),
-        "color" : int('0xffffff',16),
-        "position" : 2,
-        'permissions': '0'
+#     roleData = {
+#         "name" : str(ctx.user.username),
+#         "color" : int('0xffffff',16),
+#         "position" : 2,
+#         'permissions': '0'
     
-    }
+#     }
 
-    newrole = await bot._http.create_guild_role(guild_id=guild_id,data=roleData)
-    print("made new role")
-    newrole_id = newrole["id"]
+#     newrole = await bot._http.create_guild_role(guild_id=guild_id,data=roleData)
+#     print("made new role")
+#     newrole_id = newrole["id"]
 
-    await bot._http.add_member_role(guild_id=guild_id, user_id=ctx.user.id, role_id=newrole_id)
-    print("added new role to user")  
+#     await bot._http.add_member_role(guild_id=guild_id, user_id=ctx.user.id, role_id=newrole_id)
+#     print("added new role to user")  
 
-    sheet.update_acell(f'B{counter}',str(newrole_id))
-    print("updated data")
+#     sheet.update_acell(f'B{counter}',str(newrole_id))
+#     print("updated data")
 
 
 # load cogs
