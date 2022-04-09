9f265d0484ad7fc29a2fab642d1ea1941e79bd6c
Cas Olischläger <74181914+Toffeegaming@users.noreply.github.com>
2022-04-09 16:59:45 +0200

    Update app.py

--- bot/app.py
+++ bot/app.py
@@ -2,19 +2,19 @@ from flask import Flask
 from threading import Thread
 from werkzeug.serving import make_server
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def index():
     return "Bot is ready"
 
 def run():
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=8000)
 
 class ServerThread(Thread):
     def __init__(self, app):
         Thread.__init__(self)
-        self.server = make_server('0.0.0.0', 5000, app)
+        self.server = make_server('0.0.0.0', 8000, app)
         self.ctx = app.app_context()
         self.ctx.push()
 
