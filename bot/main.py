329289bfdd3461b2580c3391d3a4bec9c3d2d284
Cas Olischläger <74181914+Toffeegaming@users.noreply.github.com>
2022-04-09 15:02:20 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,37 +1,4 @@
-import interactions, os, sys, datetime, gspread, aiocron
-from flask import Flask
-from threading import Thread
-from werkzeug.serving import make_server
-
-class ServerThread(Thread):
-
-    def __init__(self, app):
-        Thread.__init__(self)
-        self.server = make_server('0.0.0.0', 5000, app)
-        self.ctx = app.app_context()
-        self.ctx.push()
-
-    def run(self):
-        #log.info('starting server')
-        self.server.serve_forever()
-
-    def shutdown(self):
-        self.server.shutdown()
-
-def start_server():
-    global server
-    app = Flask('myapp')
-    # App routes defined here
-    @app.route('/')
-    def index():
-        return f"Bot is ready @{datetime.datetime.utcnow()}"
-    server = ServerThread(app)
-    server.start()
-    #log.info('server started')
-
-def stop_server():
-    global server
-    server.shutdown()
+import interactions, os, sys, datetime, gspread
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir_path)
@@ -91,7 +58,6 @@ async def on_ready():
 
     time = datetime.datetime.utcnow()
     await channel.send(f"[{time}] [STARTUP] Logged in in {numberGuild} servers!{os.linesep}{list_guild_ids}")
-    start_server()
 
 @bot.event
 async def on_guild_member_add(ctx):
@@ -143,6 +109,41 @@ for filename in os.listdir(dir_path + '/cogs'):
     else:
         print(f'[COGS] Unable to load {filename}')
 
+
+import aiocron
+from flask import Flask
+from threading import Thread
+from werkzeug.serving import make_server
+
+class ServerThread(Thread):
+
+    def __init__(self, app):
+        Thread.__init__(self)
+        self.server = make_server('0.0.0.0', 5000, app)
+        self.ctx = app.app_context()
+        self.ctx.push()
+
+    def run(self):
+        print('starting server')
+        self.server.serve_forever()
+
+    def shutdown(self):
+        self.server.shutdown()
+
+def stop_server():
+    global server
+    server.shutdown()
+
+
+global server
+app = Flask('myapp')
+# App routes defined here
+@app.route('/')
+def index():
+    return f"Bot is ready @{datetime.datetime.utcnow()}"
+server = ServerThread(app)
+server.start()
+print('server started')
 
 # cronjob
 @aiocron.crontab('* * * * *')
